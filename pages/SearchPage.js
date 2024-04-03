873b32f122431cc112d891e379bcbd16e2d786cf
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-04-03 21:19:02 +0100

    search and image position styled

--- pages/SearchPage.js
+++ pages/SearchPage.js
@@ -17,56 +17,56 @@ export default function SearchPage({ navigation }) {
   const [userSearch, setUserSearch] = useState("");
 
   return (
-    <>
     <View style={styles.container}>
-        {/* <Image
-      style={styles.image}
-      source={require("../assets/white-disgo.png")}
-    /> */}
-        {/* <View>
-      <Text style={styles.locationHeaderText}>Where Would You Like To Search?</Text>
-    </View> */}
-        <View style={{ justifyContent: "flex-start", position: "absolute", zIndex:999, width: 360}}>
+      <View style={styles.searchContainer}>
         <SearchArea />
-          <SearchBar setUserSearch={setUserSearch} userSearch={userSearch} />
-        </View>
-        <View style={styles.headerWrap}>
-          <Text style={styles.headerText}>What are you in the mood for?</Text>
-        </View>
-        <View style={{alignItems: "center"}}>
-        <Image
-      source={require("../assets/donut-lady.png")} style={{width: 335, height:294}} />
+        <SearchBar setUserSearch={setUserSearch} userSearch={userSearch} />
+      </View>
+
+      <View style={styles.imageWrap}>
+        <Text style={styles.headerText}>What are you in the mood for?</Text>
+        <Image source={require("../assets/donut-lady.png")} style={styles.image} />
       </View>
     </View>
-    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: "flex",
     flexDirection: "column",
     alignContent: "center",
+    justifyContent: "flex-end",
     backgroundColor: "#3AD6A7",
     paddingBottom: 0,
   },
+  searchContainer: {
+    top: 0,
+    borderWidth: 1,
+    position: "absolute", 
+    zIndex:999, 
+    width: "100%"
+  },
+  imageWrap:{
+    alignItems: "center"
+  },
   image: {
-    width: 70,
-    height: 78,
-    objectFit: "cover"
+    width: 335, 
+    height:294
   },
-  headerWrap: {
-    alignItems: "center",
-    paddingTop:20,
-    paddingBottom: 10,
-    marginTop: 170,
-  },
-  locationHeaderText: {
-    color: "#FFF",
-    fontWeight: "bold",
-    fontSize: 25,
-    textAlign: "center",
-  },  
+  // headerWrap: {
+  //   alignItems: "center",
+  //   paddingTop:20,
+  //   paddingBottom: 10,
+  //   marginTop: 170,
+  // },
+  // locationHeaderText: {
+  //   color: "#FFF",
+  //   fontWeight: "bold",
+  //   fontSize: 25,
+  //   textAlign: "center",
+  // },  
   headerText: {
     color: "#FFF",
     fontWeight: "bold",
@@ -74,19 +74,19 @@ const styles = StyleSheet.create({
     textAlign: "center",
     width: 260,
   },
-  searchButton: {
-    width: 139,
-    height: 57,
-    backgroundColor: "#4C5B61",
-    borderRadius: 29,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  searchButtonText: {
-    color: "#FFF",
-    fontWeight: "bold",
-    fontSize: 18,
-  },
+  // searchButton: {
+  //   width: 139,
+  //   height: 57,
+  //   backgroundColor: "#4C5B61",
+  //   borderRadius: 29,
+  //   display: "flex",
+  //   flexDirection: "row",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // },
+  // searchButtonText: {
+  //   color: "#FFF",
+  //   fontWeight: "bold",
+  //   fontSize: 18,
+  // },
 });
