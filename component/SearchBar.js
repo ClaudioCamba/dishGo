a8c8d12e8c5c57d3add1810e05b06682aca31f59
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-04-08 16:22:58 +0100

    move filterd dish state inside setquery state to prevent search delay, remove unused userSearch state

--- component/SearchBar.js
+++ component/SearchBar.js
@@ -9,11 +9,11 @@ import { Searchbar, Surface } from 'react-native-paper';
 import { useNavigation } from "@react-navigation/native";
 import OutsidePressHandler from 'react-native-outside-press';
 
-export default function SearchBar({userSearch, setUserSearch}) {
+export default function SearchBar() {
     const navigation = useNavigation();
     const [dishes, setDishes] =  useState([]);
     const [filterDishes, setFilterDishes] =  useState([]);
-    const [filteredSuggestions, setFilteredSuggestions] =  useState([]);
+    // const [filteredSuggestions, setFilteredSuggestions] =  useState([]);
     const [searchQuery, setSearchQuery] = useState('');
   
   useEffect(() =>{
@@ -23,17 +23,16 @@ export default function SearchBar({userSearch, setUserSearch}) {
   },[])
  
   const onChangeText = async (e) =>{
-    setSearchQuery(e)
-    setUserSearch(e)
-    if (searchQuery.length > 1 && filterSearch(dishes, searchQuery).length > 0){
-      const filteredSearches = filterSearch(dishes, searchQuery)
-      const firstFiveSuggestions = filterSuggestions(dishes, searchQuery)
-      setFilterDishes(filteredSearches);
-      setFilteredSuggestions(firstFiveSuggestions);
-    } else {
-      setFilterDishes([])
-      setFilteredSuggestions([]);
-    }
+
+      setSearchQuery((r)=> {
+      if (e.length > 2 && filterSearch(dishes, e).length > 0){
+        setFilterDishes(filterSearch(dishes, searchQuery));
+      } else {
+        setFilterDishes([])
+      }
+
+      return e;
+    })
   }
 
   const dishSelected = (dishName) => {
@@ -43,10 +42,8 @@ export default function SearchBar({userSearch, setUserSearch}) {
   }
   
     const getItemText = (item, index) =>{
-      console.log(filteredSuggestions.length-1)
-      console.log(index)
       return (
-        <View  style={[styles.listItem, index % 2 === 0 ? styles.oddItem : styles.evenItem, index === filteredSuggestions.length-1 ? styles.lastItem: null]}>
+        <View  style={[styles.listItem, index % 2 === 0 ? styles.oddItem : styles.evenItem, index === filterDishes.length-1 ? styles.lastItem: null]}>
             <List.Icon color="#A6A6A6" icon="magnify" />
             <Text style={styles.listText}>{item.dish_name}</Text>
         </View>
@@ -54,7 +51,8 @@ export default function SearchBar({userSearch, setUserSearch}) {
   
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        <SafeAreaView style={styles.SafeAreaView}>
+          <SafeAreaView style={styles.SafeAreaView}>
+          <OutsidePressHandler onOutsidePress={() => console.log(filterDishes)}>
             <Searchbar
                 onSubmitEditing={(event) => { navigation.navigate('ResultsPage', {dish: userSearch}) }}
                 fontWeight="bold"
@@ -70,12 +68,10 @@ export default function SearchBar({userSearch, setUserSearch}) {
                 style={styles.searchBar} />
 
                 {
-                  filteredSuggestions.length > 0 ?
-                  <OutsidePressHandler
-                  onOutsidePress={() => setFilterDishes([])}>
+                  filterDishes.length > 0 ?
                   <Surface style={styles.surface} elevation={0}>
                     <FlatList
-                        data={filteredSuggestions}
+                        data={filterDishes}
                         renderItem={({ item, index}) => <Pressable onPress={() => {
                         setSearchQuery(item.dish_name);
                         dishSelected(item.dish_name);
@@ -86,9 +82,9 @@ export default function SearchBar({userSearch, setUserSearch}) {
                         showsVerticalScrollIndicator={true} 
                     />
                   </Surface> 
-                   </OutsidePressHandler>: null
+                   : null
                 }
-
+          </OutsidePressHandler>
         </SafeAreaView>
       </TouchableWithoutFeedback>
     )
@@ -115,7 +111,7 @@ const styles = StyleSheet.create({
     borderRadius: 26,
     overflow: 'hidden',
     backgroundColor:'#FFFFFF',
-    maxHeight: 270,
+    maxHeight: 240,
     width: '100%',
     // paddingBottom: 50,
   },
